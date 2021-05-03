252afb8bfc1391dc0149d36dfafc7da44037299c
Syeda Sumaia Sultana <63061055+sumaia31@users.noreply.github.com>
2021-05-04 01:19:06 +0600

    Add files via upload

---
+++ Circle.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define M_PI 3.1416
+using namespace std;
+int main()
+{
+
+    float r,area;
+    cout<<"Please enter radius of a circle: "<<endl;
+    cin>>r;
+    area=M_PI*r*r;
+
+    cout<<"area of circle is :"<<area<<endl;
+
+    return 0;
+}
