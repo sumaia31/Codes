f290f2251a4a3d05dba72223ca12a7e720969210
Syeda Sumaia Sultana <63061055+sumaia31@users.noreply.github.com>
2021-05-04 01:10:12 +0600

    Add files via upload

---
+++ Rectangle.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    float a,b,area;
+    cout<<"Please enter length and width of rectangle: "<<endl;
+    cin>>a>>b;
+    area=a*b;
+
+    cout<<"area of rectangle is :"<<area<<endl;
+
+    return 0;
+}
