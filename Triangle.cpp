29a59d8fcc44c8c728fe46503f218078c56a83b3
Syeda Sumaia Sultana <63061055+sumaia31@users.noreply.github.com>
2021-05-04 01:01:42 +0600

    Add files via upload

---
+++ Triangle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    float area,base,height;
+    cout<<"Please enter base and height: "<<endl;
+    cin>>base>>height;
+    area=0.5*base*height;
+    cout<<"area of triangle is :"<<area<<endl;
+
+    return 0;
+}
