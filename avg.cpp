7704d8b3136b5f6d6b3cf8ad2d838d8278743778
Syeda Sumaia Sultana <63061055+sumaia31@users.noreply.github.com>
2021-05-04 00:56:59 +0600

    Add files via upload

---
+++ avg.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,sum;
+    float avg;
+    cout<<"Please enter three numbers: ";
+    cin>>a>>b>>c;
+    sum=a+b+c;
+    avg=(float)sum/3;
+    cout<<"average is :"<<avg<<endl;
+
+    return 0;
+}
