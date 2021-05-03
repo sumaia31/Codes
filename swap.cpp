4276e6aa8469194d7fd841717a13a89d45652964
Syeda Sumaia Sultana <63061055+sumaia31@users.noreply.github.com>
2021-05-04 01:37:36 +0600

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int a,b,temp;
+    cout<<"Enter two numbers: "<<endl;
+    cin>>a>>b;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"after swapping :"<<"a :"<<a<<" "<<"b :"<<b<<endl;
+
+
+    return 0;
+}
+
