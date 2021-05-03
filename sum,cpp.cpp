a39a784057c2848806a212669d9e81b5f26fd4cc
Syeda Sumaia Sultana <63061055+sumaia31@users.noreply.github.com>
2021-05-04 00:52:16 +0600

    Add files via upload

---
+++ sum,cpp.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Please enter two numbers: ";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"Sum is :"<<sum<<endl;
+
+    return 0;
+}
