4fa01d7773f5ee656612874dc9abf49525811023
Syeda Sumaia Sultana <63061055+sumaia31@users.noreply.github.com>
2021-05-04 01:28:11 +0600

    Add files via upload

---
+++ Celcius to Farenhite.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    float c,f;
+    cout<<"Please enter celcius temperature: "<<endl;
+    cin>>c;
+    f=(1.8*c)+32;
+
+    cout<<"Farenhite temperature is :"<<f<<endl;
+
+    return 0;
+}
+
