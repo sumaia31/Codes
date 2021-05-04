3fbc2af2a3f1cde6476ac5be8b1273cca569ca2b
Syeda Sumaia Sultana <63061055+sumaia31@users.noreply.github.com>
2021-05-04 22:25:56 +0600

    Add files via upload

---
+++ power.cpp
@@ -0,0 +1,17 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int a,b;
+    cout<<"Enter number please "<<endl;
+    cin>>a;
+    cout<<"Enter power please "<<endl;
+    cin>>b;
+    cout<<pow(a,b)<<endl;
+
+
+    return 0;
+}
+
