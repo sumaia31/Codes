84454a88b2235bd17c3c828725039651fc41d59e
Syeda Sumaia Sultana <63061055+sumaia31@users.noreply.github.com>
2021-05-04 22:31:27 +0600

    Add files via upload

---
+++ math.cp.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    float a,b,c,d,e;
+    cout<<"Enter number please "<<endl;
+    cin>>a;
+
+    b=log(a);
+    c=log10(a);
+    d=exp(a);
+    e=sin(a);
+    cout<<b<<" "<<c<<" "<<d<<" "<<e<<endl;
+
+    return 0;
+}
+
